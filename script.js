8d68faec3bbe61837f064519327487d743df5554
NonyeReeta <nonyeibeanu@gmail.com>
2022-01-03 03:05:44 +0100

    added IDs to 'td' and event listener to fire once.

--- script.js
+++ script.js
@@ -15,16 +15,6 @@ scoreElX.textContent = 0;
 scoreElO.textContent = 0;
 scoreElTie.textContent = 0;
 
-// creating a function that checks iff all cells have been filled
-const cellFilled = function () {
-  for (let i = 0; i < cells.length; i++) {
-    if (cells[i].textContent !== "") {
-      return true;
-    } else {
-      return false;
-    }
-  }
-};
 // game init/restarting function
 const init = function () {
   gameOn = true;
@@ -40,15 +30,25 @@ init();
 // query selecting all cells in the table
 let cells = document.querySelectorAll(".columns");
 
+// function to fill the cells
+const fillCells = function (elem) {
+  let el = document.getElementById(elem);
+  if (activePlayer == 0) {
+    el.innerHTML = xMarkSrc;
+    activePlayer = 1;
+  } else {
+    el.innerHTML = oMarkSrc;
+    activePlayer = 0;
+  }
+};
 // looping through each rows and adding an event listener to each cells
 for (let i = 0; i < cells.length; i++) {
-  cells[i].addEventListener("click", function () {
-    if (activePlayer == 0 && cellFilled() === false) {
-      cells[i].innerHTML = xMarkSrc;
-      activePlayer = 1;
-    } else if (activePlayer == 1 && cellFilled() === false) {
-      cells[i].innerHTML = oMarkSrc;
-      activePlayer = 0;
-    }
-  });
+  let box = cells[i].id;
+  cells[i].addEventListener(
+    "click",
+    function () {
+      fillCells(box);
+    },
+    { once: true }
+  );
 }
